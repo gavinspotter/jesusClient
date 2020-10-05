74fbdbc9d94b07d20bd2963ae779b56e8e157dfd
Osiris <potterosiris@gmail.com>
2020-10-05 18:18:50 -0400

    label

--- src/dates/10-4-20/FormHookInput.js
+++ src/dates/10-4-20/FormHookInput.js
@@ -3,6 +3,13 @@ import React from "react";
 const FormHookInput = (props) => {
   const element =
     props.element === "input" ? <input /> : <textarea rows={props.rows || 3} />;
+
+  return (
+    <div>
+      <label>{props.label}</label>
+      {element}
+    </div>
+  );
 };
 
 export default FormHookInput;
