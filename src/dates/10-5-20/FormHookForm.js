bbdf79d71a1f27620a2ee0fbd43e9bcdfd5f00e3
Osiris <potterosiris@gmail.com>
2020-10-05 18:10:02 -0400

    import formhookinput

--- src/dates/10-5-20/FormHookForm.js
+++ src/dates/10-5-20/FormHookForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormHookInput from "../10-4-20/FormHookInput";
 
-const FormHookForm = () => {};
+const FormHookForm = () => {
+  return <FormHookInput />;
+};
 
 export default FormHookForm;
