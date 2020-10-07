c988f79aa339ca37a91d48295aa74e8b98d92d8a
Osiris <potterosiris@gmail.com>
2020-10-06 20:48:08 -0400

    import react-router-dom components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 
 import "./App.css";
 import Jesus from "./dates/9-30-20/Jesus";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return <Jesus></Jesus>;
