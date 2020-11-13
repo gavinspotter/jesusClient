3add702ad0dffede1fa6cddce67a6fcd84c0eff6
Osiris <potterosiris@gmail.com>
2020-11-12 22:33:52 -0500

    element const

--- src/dates/10-9-20/Andrew.js
+++ src/dates/10-9-20/Andrew.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Andrew = () => {};
+const Andrew = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input ref={props.hi} name={props.yo}>
+        {" "}
+      </input>
+    ) : (
+      <textarea ref={props.hi} name={props.yo} rows={props.yho || 3}></textarea>
+    );
+};
 
 export default Andrew;
