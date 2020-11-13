9ed27d057c94d4ded210767a20909fe1b9db1595
Osiris <potterosiris@gmail.com>
2020-11-12 22:35:36 -0500

    andrew return

--- src/dates/10-9-20/Andrew.js
+++ src/dates/10-9-20/Andrew.js
@@ -3,12 +3,18 @@ import React from "react";
 const Andrew = (props) => {
   const element =
     props.element === "input" ? (
-      <input ref={props.hi} name={props.yo}>
-        {" "}
-      </input>
+      <input ref={props.hi} name={props.yo} />
     ) : (
       <textarea ref={props.hi} name={props.yo} rows={props.yho || 3}></textarea>
     );
+
+  return (
+    <div>
+      <label>{props.label}</label>
+      <br />
+      {element}
+    </div>
+  );
 };
 
 export default Andrew;
