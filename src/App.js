42f61280a9325ecb673949024dfc75363ff9f395
Osiris <potterosiris@gmail.com>
2020-11-14 22:20:03 -0500

    bartholaew imp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.css";
 import Jesus from "./dates/9-30-20/Jesus";
+import Bartholamew from "./dates/10-9-20/Bartholamew";
 import {
   BrowserRouter as Router,
   Route,
@@ -10,7 +11,7 @@ import {
 } from "react-router-dom";
 
 function App() {
-  return <Router></Router>;
+  return;
 }
 
 export default App;
