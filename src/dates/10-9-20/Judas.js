8a2a50852ede2ac513b7868587cc89dc2bd14a4d
Osiris <potterosiris@gmail.com>
2020-11-14 22:13:48 -0500

    element const judas

--- src/dates/10-9-20/Judas.js
+++ src/dates/10-9-20/Judas.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Judas = () => {};
+const Judas = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input ref={props.ref} name={props.name} />
+    ) : (
+      <textarea
+        ref={props.ref}
+        name={props.name}
+        rows={props.rows || 3}
+      ></textarea>
+    );
+};
 
 export default Judas;
