7ae7ba0b876470c986cf4d714c8dc947ea00069a
Osiris <potterosiris@gmail.com>
2020-11-16 22:12:56 -0500

    matthew element const

--- src/dates/10-9-20/Matthew.js
+++ src/dates/10-9-20/Matthew.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Matthew = () => {};
+const Matthew = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input ref={props.Ref} name={props.name} />
+    ) : (
+      <textarea
+        ref={props.Ref}
+        name={props.name}
+        rows={props.rows || 3}
+      ></textarea>
+    );
+};
 
 export default Matthew;
