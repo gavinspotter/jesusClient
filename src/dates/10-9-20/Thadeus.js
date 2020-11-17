8d2b59a820b52b91d3dba7544579641db2bfb30d
Osiris <potterosiris@gmail.com>
2020-11-16 22:24:52 -0500

    matthew import

--- src/dates/10-9-20/Thadeus.js
+++ src/dates/10-9-20/Thadeus.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import Matthew from "./Matthew";
 
 const Thadeus = () => {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Matthew Ref={register} name="lol" />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Thadeus;
